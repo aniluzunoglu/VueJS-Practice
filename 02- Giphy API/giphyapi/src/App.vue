7c4a6e91d1277220ac06cfda005d9b6ef8b6a162
aniluzunoglu <aniluzunoglu@outlook.com>
2021-01-31 15:58:34 +0300

    Webpack project created

---
+++ 02- Giphy API/giphyapi/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <search></search>
+    <preview v-bind:gifs="gifs"></preview> 
+  </div>
+</template>
+
+<script>
+import Preview from "./components/preview.vue";
+import search from "./components/search.vue";
+export default {
+  name: "app",
+  components: { search, Preview },
+  data() {
+    gifs: [];
+  },
+  created() {
+    fetch(
+      "https://api.giphy.com/v1/gifs/trending?api_key=Koc5Zh8obOmjL4Oj88g2kV8da46wI3P6&limit=25&rating=g"
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        this.gifs = res.data;
+        console.log(res.data);
+      });
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
